f48d6a2a7f30321a0d6c21bf9d482dda324a4644
Jamesl3483 <jlukas6249@gmail.com>
2023-07-01 10:37:05 -0700

    tried to modify media

--- app/src/main/java/com/example/ustock/PostView.kt
+++ app/src/main/java/com/example/ustock/PostView.kt
@@ -8,9 +8,19 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.util.Date
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.layout.ContentScale
+import coil.compose.rememberImagePainter
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ui.PlayerView
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.foundation.Image
+
 
 
 @Composable
@@ -28,9 +38,41 @@ fun PostItem(post: Post) {
         Column(modifier = Modifier.padding(16.dp)) {
             Text(text = "ID: ${post.id}")
             Text(text = "Caption: ${post.caption}")
-            Text(text = "Media Type: ${post.media.type}")
-            Text(text = "Media Content: ${post.media.content ?: "No content"}")
-            Text(text = "Media URL: ${post.media.url ?: "No URL"}")
+            val context = LocalContext.current
+
+            when (post.media.type) {
+                "image" -> {
+                    val painter = rememberImagePainter(
+                        data = post.media.url,
+                        builder = {
+                            crossfade(true)
+                        }
+                    )
+                    Image(
+                        painter = painter,
+                        contentDescription = "Post image",
+                        modifier = Modifier.fillMaxWidth(),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+                "video" -> {
+                    AndroidView(
+                        factory = {
+                            PlayerView(it).apply {
+                                useController = false
+                                player = SimpleExoPlayer.Builder(it).build().also { player ->
+                                    val mediaItem = MediaItem.fromUri(post.media.url ?: "No URL")
+                                    player.setMediaItem(mediaItem)
+                                    player.prepare()
+                                }
+                            }
+                        },
+                        modifier = Modifier.fillMaxWidth().aspectRatio(16 / 9f)
+                    )
+                }
+                "text" -> Text(text = post.media.content ?: "No content")
+                else -> Text("Unsupported media type: ${post.media.type}")
+            }
             Text(text = "User: ${post.user}")
             Text(text = "Aspects: ${post.aspects?.joinToString() ?: "No aspects"}")
             Text(text = "Upvotes: ${post.upvotes?.joinToString() ?: "No upvotes"}")
