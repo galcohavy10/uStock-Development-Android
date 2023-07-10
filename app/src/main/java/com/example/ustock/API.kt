b3ff9a7a2a1a9445aa01b94528a7aa0d3b499bb8
Jamesl3483 <jlukas6249@gmail.com>
2023-07-10 10:46:19 -0700

    Problems with CoroutineScope, but groundwork has been laid

--- app/src/main/java/com/example/ustock/API.kt
+++ app/src/main/java/com/example/ustock/API.kt
@@ -1,5 +1,10 @@
 package com.example.ustock
 
+
+import kotlinx.serialization.*
+import java.net.HttpURLConnection
+import java.net.URL
+import java.io.*
 import kotlinx.serialization.json.Json
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -10,6 +15,11 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import okhttp3.*
+import android.graphics.BitmapFactory
+import android.graphics.Bitmap
+import android.util.Base64
 
 //This is a public constant to use in later APIs. All APIs use same server URL
 object Constants {
@@ -21,16 +31,40 @@ class API {
     private val client = OkHttpClient() //reduced complexity to all use this client.
     private val json = Json { ignoreUnknownKeys = true }
 
-    private fun createPostRequest(endpoint: String, body: String): Request {
-        val url = "${Constants.SERVER_URL}$endpoint"
-        val requestBody = body.toRequestBody("application/json".toMediaType())
-        return Request.Builder().url(url).post(requestBody).build()
+    private suspend fun makeRequest(method: String, endpoint: String, body: String? = null): String = withContext(Dispatchers.IO) {
+        val url = URL("$server$endpoint")
+        with(url.openConnection() as HttpURLConnection) {
+            requestMethod = method
+            setRequestProperty("Content-Type", "application/json")
+            setRequestProperty("Accept", "application/json")
+
+            if (body != null) {
+                doOutput = true
+                val outputWriter = OutputStreamWriter(outputStream)
+                outputWriter.write(body)
+                outputWriter.flush()
+            }
+
+            inputStream.bufferedReader().readText()
+        }
     }
 
-    //Suspend keyword used to make this action non-blocking.
-    //The main thread may be occupied with a user action and this is helpful async code.
-    private suspend fun makeRequest(request: Request): String = withContext(Dispatchers.IO) {
-        val response = client.newCall(request).execute()
+    suspend fun getMyPosts(userID: String): List<Post> {
+        val endpoint = "/api/getMyPosts"
+        val body = mapOf("userID" to userID)
+
+        val requestBody = JSONObject(body).toString()
+            .toRequestBody("application/json".toMediaType())
+
+        val request = Request.Builder()
+            .url(server + endpoint)
+            .post(requestBody)
+            .build()
+
+        val response = withContext(Dispatchers.IO) {
+            client.newCall(request).execute()
+        }
+
         if (!response.isSuccessful) throw Exception("Server Error: ${response.code}")
         response.body?.string() ?: throw Exception("Invalid response")
     }
@@ -48,11 +82,115 @@ class API {
         val body = json.encodeToString(post)
         val request = createPostRequest("/api/sendPost", body)
         return try {
-            makeRequest(request)
+            makeRequest("POST", endpoint, body)
             true
         } catch (e: Exception) {
             println("Error sending post: ${e.localizedMessage}")
             false
         }
     }
+
+    suspend fun getUserID(username: String, password: String): Result<String> {
+        val endpoint = "/api/getUserID"
+        val body = mapOf("username" to username, "password" to password)
+        val bodyString = JSONObject(body).toString()
+        val requestBody = bodyString.toRequestBody("application/json".toMediaType())
+
+        val request = Request.Builder()
+            .url(server + endpoint)
+            .post(requestBody)
+            .build()
+
+        return withContext(Dispatchers.IO) {
+            try {
+                client.newCall(request).execute().use { response ->
+                    if (!response.isSuccessful) {
+                        Result.failure(Throwable("Server Error: ${response.code}"))
+                    } else {
+                        val userID = response.body?.string() ?: throw Exception("Invalid response")
+                        if (userID.isNotEmpty()) {
+                            Result.success(userID)
+                        } else {
+                            Result.failure(Throwable("Invalid JSON"))
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                Result.failure(e)
+            }
+        }
+    }
+
+    //Fetch Media from API
+    //get any image, this is getting the whole file. the response will look like base64 data.
+    // Required imports for OkHttp, JSONObject, Bitmap, and Base64
+
+    fun fetchImageData(postID: String): Result<Bitmap> {
+        val endpoint = "/api/fetchImageData"
+        val formBody = FormBody.Builder()
+            .add("postID", postID)
+            .build()
+
+        val request = Request.Builder()
+            .url(endpoint)
+            .post(formBody)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val jsonResponse = JSONObject(response.body?.string())
+            val base64Image = jsonResponse.optString("base64Image")
+
+            val bytes = Base64.decode(base64Image, Base64.DEFAULT)
+            val image = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+
+            completion(Result.success(image))
+        }
+    }
+
+    fun fetchVideoURL(postID: String):Result<URL> {
+        val endpoint = "/api/fetchVideoAuthorizedURL"
+        val formBody = FormBody.Builder()
+            .add("postID", postID)
+            .build()
+
+        val request = Request.Builder()
+            .url(endpoint)
+            .post(formBody)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val jsonResponse = JSONObject(response.body?.string())
+            val urlString = jsonResponse.optString("url")
+            val url = URL(urlString)
+
+            completion(Result.success(url))
+        }
+    }
+
+    fun fetchAudioURL(postID: String):Result<URL>  {
+        val endpoint = "/api/fetchAudioAuthorizedURL"
+        val formBody = FormBody.Builder()
+            .add("postID", postID)
+            .build()
+
+        val request = Request.Builder()
+            .url(endpoint)
+            .post(formBody)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+            val jsonResponse = JSONObject(response.body?.string())
+            val urlString = jsonResponse.optString("url")
+            val url = URL(urlString)
+
+            completion(Result.success(url))
+        }
+    }
+
 }
--- app/src/main/java/com/example/ustock/PostView.kt
+++ app/src/main/java/com/example/ustock/PostView.kt
@@ -1,5 +1,6 @@
 package com.example.ustock
 
+import android.graphics.Bitmap
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,16 +14,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import java.util.Date
 import androidx.compose.foundation.layout.*
-import androidx.compose.ui.layout.ContentScale
-import coil.compose.rememberImagePainter
-import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.ui.PlayerView
-import androidx.compose.ui.viewinterop.AndroidView
-import androidx.compose.foundation.Image
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowCircleDown
+import androidx.compose.material.icons.filled.ArrowCircleUp
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
-
-
+//Compile all the individual Postitems into a scrollable list
 @Composable
 fun PostView(posts: List<Post>) {
     LazyColumn {
@@ -32,61 +39,133 @@ fun PostView(posts: List<Post>) {
     }
 }
 
+//View one post
 @Composable
 fun PostItem(post: Post) {
     Card(modifier = Modifier.padding(8.dp)) {
         Column(modifier = Modifier.padding(16.dp)) {
+            val isUpvoted = remember { mutableStateOf(false) }
+            val isDownvoted = remember { mutableStateOf(false) }
+
             Text(text = "ID: ${post.id}")
             Text(text = "Caption: ${post.caption}")
             val context = LocalContext.current
 
+            val mediaUrl = post.media.url ?: null
+            var postImage: Bitmap? = null
+            var postVideoURL: String? = null
+            var postAudioURL: String? = null
+            var api = API()
+
             when (post.media.type) {
                 "image" -> {
-                    val painter = rememberImagePainter(
-                        data = post.media.url,
-                        builder = {
-                            crossfade(true)
-                        }
-                    )
-                    Image(
-                        painter = painter,
-                        contentDescription = "Post image",
-                        modifier = Modifier.fillMaxWidth(),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-                "video" -> {
-                    AndroidView(
-                        factory = {
-                            PlayerView(it).apply {
-                                useController = false
-                                player = SimpleExoPlayer.Builder(it).build().also { player ->
-                                    val mediaItem = MediaItem.fromUri(post.media.url ?: "No URL")
-                                    player.setMediaItem(mediaItem)
-                                    player.prepare()
+                    if (postImage == null) {
+                        //imageLoading = true
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val result = api.fetchImageData(post.id)
+                            withContext(Dispatchers.Main) {
+                                //imageLoading = false
+                                if (result.isSuccess) {
+                                    val image = result.getOrNull()
+                                    postImage = image
+                                } else if (result.isFailure) {
+                                    println("Failed to fetch image data: ${result.exceptionOrNull()}")
                                 }
                             }
-                        },
-                        modifier = Modifier.fillMaxWidth().aspectRatio(16 / 9f)
-                    )
+                        }
+                    }
                 }
+
+                "video" -> {
+                    if (postVideoURL == null) {
+                        //videoLoading = true
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val result = api.fetchVideoURL(post.id)
+                            withContext(Dispatchers.Main) {
+                                //videoLoading = false
+                                if (result.isSuccess) {
+                                    val url = result.getOrNull()
+                                    postVideoURL = url
+                                } else if (result.isFailure) {
+                                    println("Failed to fetch video URL: ${result.exceptionOrNull()}")
+                                }
+
+                            }
+                        }
+                    }
+                }
+
+                "audio" -> {
+                    if (postAudioURL == null) {
+                        //audioLoading = true
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val result = api.fetchAudioURL(post.id)
+                            withContext(Dispatchers.Main) {
+                                //audioLoading = false
+                                if (result.isSuccess) {
+                                    val url = result.getOrNull()
+                                    postAudioURL = url
+                                } else if (result.isFailure) {
+                                    println("Failed to fetch audio URL: ${result.exceptionOrNull()}")
+                                }
+                            }
+                        }
+                    }
+                }
+
                 "text" -> Text(text = post.media.content ?: "No content")
                 else -> Text("Unsupported media type: ${post.media.type}")
             }
+
+            //TODO Ask what user does
             Text(text = "User: ${post.user}")
-            Text(text = "Aspects: ${post.aspects?.joinToString() ?: "No aspects"}")
-            Text(text = "Upvotes: ${post.upvotes?.joinToString() ?: "No upvotes"}")
-            Text(text = "Downvotes: ${post.downvotes?.joinToString() ?: "No downvotes"}")
+            //TODO show the aspects as a list and probably need to decode it later
+            Text(text = "Number of Aspects: ${post.aspects?.size ?: "No aspects"}")
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Row {
+                    //TODO: Make the application remember the upvote with the user ID
+                    //TODO: If one button is pressed, make sure the other button is off. (Both cannot be on)
+                    Button(
+                        onClick = { isUpvoted.value = !isUpvoted.value },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = if (isUpvoted.value) Color.Green else Color.White),
+                        //  modifier = Modifier.padding(8.dp)
+                    ) {
+                        Icon(Icons.Filled.ArrowCircleUp, contentDescription = null)
+                        Text(text = "${post.upvotes?.size ?: "0"}")
+                    }
+
+                    //TODO: Make the button red when pushed
+                    Button(
+                        onClick = { isDownvoted.value = !isDownvoted.value },
+                        colors = ButtonDefaults.buttonColors(backgroundColor = if (isDownvoted.value) Color.Green else Color.White)
+                    ) {
+                        Icon(Icons.Filled.ArrowCircleDown, contentDescription = null)
+                        Text(text = "${post.downvotes?.size ?: "0"}")
+                    }
+                }
+
+                Button(
+                    onClick = { /*TODO: handle click event*/ },
+                    modifier = Modifier.size(50.dp)
+                ) {
+                    Text(text = "Comments: ${post.comments?.size ?: "0"}")
+                }
+            }
             Text(text = "Created at: ${post.createdAt}")
-            Text(text = "Updated at: ${post.updatedAt ?: "Not updated"}")
-            Text(text = "Comments: ${post.comments?.joinToString() ?: "No comments"}")
+
+
             Text(text = "Tags: ${post.tags?.joinToString() ?: "No tags"}")
             Text(text = "Mentions: ${post.mentions?.joinToString() ?: "No mentions"}")
         }
     }
 }
 
-
+//Easy debug function
+//However does not work now since it is unable to parse Glide. TBH, idk if glide is working
 @Preview(showBackground = true)
 @Composable
 fun PreviewPostView() {
@@ -104,7 +183,7 @@ fun PreviewPostView() {
             user = "User 1",
             aspects = listOf("1", "2"),
             upvotes = listOf("1", "2"),
-            downvotes = listOf("1", "2"),
+            downvotes = listOf(),
             createdAt = Date(),
             updatedAt = Date(),
             comments = listOf("1", "2"),
